7db13fa5567c667c8dfb34ab624316f0c3a36e94
Shri Om Trivedi <tshriom@gmail.com>
2022-02-20 22:15:34 +0530

    git branch testing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Home from "./pages/home/Home";
 function App() {
   return (
     <div className='App'>
-      <Home />
-      {/* <Profile /> */}
+      {/* <Home /> */}
+      <Profile />
     </div>
   );
 }
