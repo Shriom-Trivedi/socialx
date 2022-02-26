758a793f8a2d3209e0cfe12debfa20bb86a27434
Shri Om Trivedi <tshriom@gmail.com>
2022-02-26 22:09:53 +0530

    profile details are added

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Feed from "../feed/Feed";
-import Rightbar from "../rightbar/Rightbar";
-import Sidebar from "../sidebar/Sidebar";
 import Topbar from "../topbar/Topbar";
+import { MoreVert } from "@mui/icons-material";
+import { Tooltip } from "@mui/material";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import DateRangeIcon from "@mui/icons-material/DateRange";
 import "./profile.css";
 
 const Profile = () => {
@@ -10,7 +11,76 @@ const Profile = () => {
     <>
       <Topbar />
       <div className='profileContainer'>
+        <div className='profileTop'>
+          <div className='profileCover'>
+            <img src='assets/post/3.jpeg' alt='' className='profileCoverImg' />
+          </div>
+        </div>
 
+        <div className='profileCenter'>
+          <div className='profileCenterWrapper'>
+            <div className='profileCenterTop'>
+              <div className='profileUser'>
+                <img
+                  src='assets/person/1.jpeg'
+                  alt=''
+                  className='profileUserImg'
+                />
+              </div>
+              <div className='profileUserFollow'>
+                <div className='followLists'>
+                  <p className='followers'>290 followers</p>
+                  <p className='followers'>263 followings</p>
+                </div>
+                <button className='messageBtn'>Message</button>
+                <button className='followBtn'>Follow</button>
+                <Tooltip title='More' arrow>
+                  <div className='postTopRight'>
+                    <MoreVert />
+                  </div>
+                </Tooltip>
+              </div>
+            </div>
+            <div className='profileCenterBottom'>
+              <div className='profileName'>
+                <p className='profileUserName'>Shri Om Trivedi</p>
+                <img src='assets/verify.png' alt='' className='verify' />
+              </div>
+              <div className='profileUserInfo'>
+                <p className='username'>@shriomatic</p>
+                <div className='location'>
+                  <FmdGoodIcon
+                    style={{
+                      fontSize: "13px",
+                      marginRight: "5px",
+                      color: "gray",
+                    }}
+                  />
+                  <p>Pune</p>
+                </div>
+                <div className='joinDate'>
+                  <DateRangeIcon
+                    style={{
+                      fontSize: "13px",
+                      marginRight: "5px",
+                      color: "gray",
+                    }}
+                  />
+                  <p>Joined Feb 2022</p>
+                </div>
+              </div>
+              <div className='profileBio'>
+                <p className='bio'>
+                  Trained Developer. Untrained Investor. Coming Enterpreneur
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='profileBottom'>
+          
+        </div>
       </div>
     </>
   );
