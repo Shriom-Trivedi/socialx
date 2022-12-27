15d719231288cfaccf03dc1c12acd46a3779be2e
Shri Om Trivedi <tshriom@gmail.com>
2022-12-28 00:41:17 +0530

    expiration date for access token updated

--- token/generateToken.js
+++ token/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateAccessToken = (user) => {
   return jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN_SECRET_KEY, {
-    expiresIn: '2000s',
+    expiresIn: '15d',
   });
 };
 
