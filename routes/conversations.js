754f38e0eb496f8ab3ae260e13abb6eb4b302b9d
Shri Om Trivedi <tshriom@gmail.com>
2023-02-16 22:52:53 +0530

    chatroom code added

--- routes/conversations.js
+++ routes/conversations.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Conversation = require('../models/Conversation');
+const Users = require('../models/Users');
 
 // Create new conversation
 router.post('/', async (req, res) => {
@@ -26,6 +27,22 @@ router.get('/:userId', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+// Get chatroom data.
+router.get('/room', async (req, res) => {
+  console.log('HELLO WORLD');
+  const { userId } = req.query;
+  const { conversationId } = req.body;
+  const user = await Users.findOne(userId);
+  //   Return if user not exist
+  if (!user) res.status(404).json('Not Found');
+
+  const conversation = Conversation.findOne(conversationId);
+  console.log(conversation);
+
+  //   TODO: Something is FUCKED here.
+  return res.status(200).json(user);
 });
 
 module.exports = router;
